a91d70879cf0f8ddfddef639ba06583774f2e2de
Descartesr1cosa <xuke31415@163.com>
2026-02-02 17:14:42 +0800

    Area vector(norm) should be calculated by ngg ( number of ghost grid) rather than ngg -1

--- src/3_field/4_MPCNS_Geometry.cpp
+++ src/3_field/4_MPCNS_Geometry.cpp
@@ -4,9 +4,9 @@
 void Field::build_geometry(int geomtry_ghost_)
 {
     register_field(FieldDescriptor{"Jac", StaggerLocation::Cell, 1, geomtry_ghost_ - 1});
-    register_field(FieldDescriptor{"JDxi", StaggerLocation::FaceXi, 3, geomtry_ghost_ - 1});
-    register_field(FieldDescriptor{"JDet", StaggerLocation::FaceEt, 3, geomtry_ghost_ - 1});
-    register_field(FieldDescriptor{"JDze", StaggerLocation::FaceZe, 3, geomtry_ghost_ - 1});
+    register_field(FieldDescriptor{"JDxi", StaggerLocation::FaceXi, 3, geomtry_ghost_});
+    register_field(FieldDescriptor{"JDet", StaggerLocation::FaceEt, 3, geomtry_ghost_});
+    register_field(FieldDescriptor{"JDze", StaggerLocation::FaceZe, 3, geomtry_ghost_});
 
     // --- NEW: covariant basis vectors at cell centers ---
     register_field(FieldDescriptor{"a_xi", StaggerLocation::Cell, 3, 0});
