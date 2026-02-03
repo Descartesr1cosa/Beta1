9be3f2f0c1917ce2427820c712d04cd4fc89b918
Descartesr1cosa <xuke31415@163.com>
2026-02-03 17:26:56 +0800

    if the program can stop, it should output the residual for the last time to prevent silent crash.

--- Z5_Mercury/include/3_Control.h
+++ Z5_Mercury/include/3_Control.h
@@ -53,6 +53,7 @@ public:
         if (maximum_time_ > 0.0 && run_data.time >= maximum_time_)
             if_stop = true;
         if_outfile = if_outfile || if_stop;
+        if_outres = if_outres || if_stop;
     }
 
 public:
