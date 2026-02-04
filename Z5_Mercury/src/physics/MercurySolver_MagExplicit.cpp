79c6a46c9161eafdf0b6646983d891daf67af8af
Descartesr1cosa <xuke31415@163.com>
2026-02-04 17:08:29 +0800

    Temporary close the ambipolar electric effects

--- Z5_Mercury/src/physics/MercurySolver_MagExplicit.cpp
+++ Z5_Mercury/src/physics/MercurySolver_MagExplicit.cpp
@@ -7,7 +7,7 @@ void MercurySolver::Build_E_explicit_edge_()
 
     AddHallEdgeEMF_();
 
-    AddAmbipolarEdgeEMF_();
+    // AddAmbipolarEdgeEMF_();
 
     // 后续CT只会用到inner的电场，不会使用nghost区域，因此只需对Pole处理即可
     // bound_.add_Edge_pole_boundary("E_xi"); // pole边界处理
