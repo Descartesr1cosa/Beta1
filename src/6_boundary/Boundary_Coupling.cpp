5f955db1a6773735aa0f6ae4e4d98548c07eb293
Descartesr1cosa <xuke31415@163.com>
2026-02-11 14:56:01 +0800

    Add the problematic field_name string to abort message

--- src/6_boundary/Boundary_Coupling.cpp
+++ src/6_boundary/Boundary_Coupling.cpp
@@ -18,7 +18,7 @@ void BoundaryCore::RegisterCoupling(const std::string &src,
     const int fid = fld_->field_id(dst_field_name);
     const auto &desc = fld_->descriptor(fid);
     if (desc.location != loc)
-        ERROR::Abort("[BoundaryCore] RegisterCoupling: dst_field location mismatch");
+        ERROR::Abort("[BoundaryCore] RegisterCoupling: dst_field location mismatch: " + dst_field_name);
 
     BOUND::CouplingKey k{src, dst, loc, channel_tag, dst_field_name};
     cpl_reg_[k] = std::move(h);
