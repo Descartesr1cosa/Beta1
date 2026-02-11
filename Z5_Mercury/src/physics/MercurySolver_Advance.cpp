c58b1e57c73c50e8f58324a4ea8f8d3365868a0f
Descartesr1cosa <xuke31415@163.com>
2026-02-11 12:37:15 +0800

    Time_advance should use their own box of range.

--- Z5_Mercury/src/physics/MercurySolver_Advance.cpp
+++ Z5_Mercury/src/physics/MercurySolver_Advance.cpp
@@ -87,39 +87,59 @@ void MercurySolver::ApplyUpdate_Euler_()
         FieldBlock &RHSB_eta = fld_->field(fid_.fid_RHS_b.eta, ib);
         FieldBlock &RHSB_zeta = fld_->field(fid_.fid_RHS_b.zeta, ib);
 
-        Int3 lo = Jac.inner_lo();
-        Int3 hi = Jac.inner_hi();
+        Int3 lo; //= Jac.inner_lo();
+        Int3 hi; //= Jac.inner_hi();
 
         if (RHSH.is_allocated())
+        {
+            lo = UH.inner_lo();
+            hi = UH.inner_hi();
             for (int i = lo.i; i < hi.i; ++i)
                 for (int j = lo.j; j < hi.j; ++j)
                     for (int k = lo.k; k < hi.k; ++k)
                         for (int m = 0; m < 5; ++m)
                             UH(i, j, k, m) += dt * RHSH(i, j, k, m);
+        }
 
         if (RHSN.is_allocated())
+        {
+            lo = UN.inner_lo();
+            hi = UN.inner_hi();
             for (int i = lo.i; i < hi.i; ++i)
                 for (int j = lo.j; j < hi.j; ++j)
                     for (int k = lo.k; k < hi.k; ++k)
                         for (int m = 0; m < 5; ++m)
                             UN(i, j, k, m) += dt * RHSN(i, j, k, m);
+        }
 
         if (RHSB_xi.is_allocated())
+        {
+            lo = Ub_xi.inner_lo();
+            hi = Ub_xi.inner_hi();
             for (int i = lo.i; i < hi.i; ++i)
                 for (int j = lo.j; j < hi.j; ++j)
                     for (int k = lo.k; k < hi.k; ++k)
                         Ub_xi(i, j, k, 0) += dt * RHSB_xi(i, j, k, 0);
+        }
 
         if (RHSB_eta.is_allocated())
+        {
+            lo = Ub_eta.inner_lo();
+            hi = Ub_eta.inner_hi();
             for (int i = lo.i; i < hi.i; ++i)
                 for (int j = lo.j; j < hi.j; ++j)
                     for (int k = lo.k; k < hi.k; ++k)
                         Ub_eta(i, j, k, 0) += dt * RHSB_eta(i, j, k, 0);
+        }
 
         if (RHSB_zeta.is_allocated())
+        {
+            lo = Ub_zeta.inner_lo();
+            hi = Ub_zeta.inner_hi();
             for (int i = lo.i; i < hi.i; ++i)
                 for (int j = lo.j; j < hi.j; ++j)
                     for (int k = lo.k; k < hi.k; ++k)
                         Ub_zeta(i, j, k, 0) += dt * RHSB_zeta(i, j, k, 0);
+        }
     }
 }
